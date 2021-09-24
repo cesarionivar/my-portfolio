807bc9d0030fb0843d2a0265559f45bcf2221305
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-09-23 21:44:41 -0400

    404 page done

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import noMatchPicture from '../assets/img/NoMatch.png';
 
 export const NoMatch = () => {
   return (
-    <div>
-      <h2>Error 404, page not found</h2>
+    <div className='noMatch'>
+      <img src={noMatchPicture} alt='No Match' />
+      <h2>Error 404, resource not found!!!</h2>
+      <Link to='/' className='btn-back'>
+        Go back &nbsp;<i class='fas fa-undo'></i>
+      </Link>
     </div>
   );
 };
