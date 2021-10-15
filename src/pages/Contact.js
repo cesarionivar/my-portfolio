3e2f744dd82a12f0ff16c99d3cb65d5a0af6d8c4
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-10-15 15:26:24 -0400

    fetch contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,13 +3,31 @@ import { Footer } from '../components/Footer/Footer';
 import { Navbar } from '../components/Navbar/Navbar';
 
 export const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let myForm = document.getElementById('contact');
+    let formData = new FormData(myForm);
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log('Form successfully submitted'))
+      .catch((error) => alert(error));
+  };
+
   return (
     <div>
       <Navbar />
       <div className='contact page'>
         <h2 className='page__title'>Contact</h2>
         <div className='contactForm__container'>
-          <form name='Contact' method='POST' netlify>
+          <form
+            onSubmit={handleSubmit}
+            id='contact'
+            name='Contact'
+            data-netlify='true'
+          >
             <div className='formControl sender-data'>
               <input name='name' type='text' placeholder='Full name' required />
               <input type='text' placeholder='Email' required />
